7303f2906754c40e99d708af777ee1df86ae3b1b
wzolichowski <wzolichowski@gmail.com>
2025-07-24 11:45:09 +0200

    test

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import List, Optional
-import os
+from typing import List
 
 
 app = FastAPI()
 
-class Todo(BaseModel):
-    id: int
+class TodoCreate(BaseModel):
     title: str
     completed: bool = False
+
+class Todo(TodoCreate):
+    id: int
+
 
 todos: List[Todo] = []
 next_id: int = 1
@@ -23,12 +25,12 @@ def get_todos():
     return [todo.dict() for todo in todos]
 
 @app.post("/todos")
-def create_todo(todo: Todo):
+def create_todo(todo: TodoCreate):
     global next_id
-    todo.id = next_id
-    todos.append(todo)
+    new_todo = Todo(id=next_id, title=todo.title, completed=todo.completed)
+    todos.append(new_todo)
     next_id += 1
-    return todo.dict()
+    return new_todo.dict()
 
 @app.put("/todos/{todo_id}")
 def update_todo(todo_id: int, update: Todo):
